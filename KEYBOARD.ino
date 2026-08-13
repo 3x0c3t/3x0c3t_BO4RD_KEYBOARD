fb7a98b7a3a6553c30a72ab20fa35a6bdcca2e54
3x0c3t <jcalonne.prv@gmail.com>
2026-08-13 02:24:22 +0200

    Fix keyboard TFT eSPI

--- KEYBOARD.ino
+++ KEYBOARD.ino
@@ -1,70 +1,75 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
 #include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_ILI9341.h>
 
 #include "config.h"
 #include "keyboard.h"
 #include "touch_calibration.h"
 
-#ifndef TFT_CS
-#define TFT_CS 15
-#endif
-
-#ifndef TFT_DC
-#define TFT_DC 2
-#endif
-
-#ifndef TFT_RST
-#define TFT_RST 16
-#endif
-
-Adafruit_ILI9341 tft(TFT_CS, TFT_DC, TFT_RST);
+TFT_eSPI tft = TFT_eSPI();
 
 void setup()
 {
     Serial.begin(115200);
-    delay(500);
+    delay(1000);
 
     Serial.println();
     Serial.println("==============================");
     Serial.println("  3x0c3t KEYBOARD v1.0");
     Serial.println("==============================");
 
-    SPI.begin();
-
     Serial.println("[KEYBOARD] TFT Init");
 
-    tft.begin();
-    tft.setRotation(2);
-
-    tft.fillScreen(ILI9341_BLACK);
-
-    delay(300);
-
-    tft.fillScreen(ILI9341_BLUE);
-
-    delay(300);
-
-    tft.fillScreen(ILI9341_BLACK);
-
-    tft.setTextColor(ILI9341_WHITE);
-    tft.setTextSize(2);
-    tft.setCursor(20, 20);
-    tft.print("3x0c3t KEYBOARD");
-
-    tft.setTextSize(1);
-    tft.setCursor(20, 50);
-    tft.print("TFT OK");
+    tft.init();
 
     Serial.println("[KEYBOARD] TFT OK");
 
+    tft.setRotation(2);
+
+    Serial.println("[KEYBOARD] Rotation OK");
+
+    tft.fillScreen(TFT_BLACK);
+
+    Serial.println("[KEYBOARD] Ecran noir OK");
+
+    delay(500);
+
+    Serial.println("[KEYBOARD] Test ROUGE");
+    tft.fillScreen(TFT_RED);
+    delay(500);
+
+    Serial.println("[KEYBOARD] Test VERT");
+    tft.fillScreen(TFT_GREEN);
+    delay(500);
+
+    Serial.println("[KEYBOARD] Test BLEU");
+    tft.fillScreen(TFT_BLUE);
+    delay(500);
+
+    Serial.println("[KEYBOARD] Test NOIR");
+    tft.fillScreen(TFT_BLACK);
+
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.setTextSize(2);
+    tft.setCursor(20, 20);
+    tft.println("3x0c3t");
+    tft.println();
+    tft.println("KEYBOARD");
+    tft.println();
+    tft.println("TFT OK");
+
+    Serial.println("[TOUCH] Init");
+
     touchCalibrationBegin();
+
+    Serial.println("[TOUCH] OK");
+
+    Serial.println("[KEYBOARD] Initialisation");
 
     keyboardBegin();
 
-    Serial.println("[KEYBOARD] Pret");
     Serial.println("[KEYBOARD] OK");
+
     Serial.println("==============================");
     Serial.println("  KEYBOARD READY");
     Serial.println("==============================");
@@ -72,16 +77,11 @@ void setup()
 
 void loop()
 {
-    int16_t x = -1;
-    int16_t y = -1;
+    int16_t x;
+    int16_t y;
 
     if (touchReadScreen(&x, &y))
     {
-        Serial.print("[TOUCH] X=");
-        Serial.print(x);
-        Serial.print(" Y=");
-        Serial.println(y);
-
         keyboardUpdate(x, y);
     }
 
